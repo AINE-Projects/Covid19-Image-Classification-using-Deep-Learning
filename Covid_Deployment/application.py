c2689295cf474566ddc2aaf90fd76af86e2fa5b8
agni1984 <agni1984@gmail.com>
2021-10-26 15:21:50 +0530

    fixes

--- Covid_Deployment/application.py
+++ Covid_Deployment/application.py
@@ -17,7 +17,7 @@ def success():
         #file_path="E:\aine\project9\Project9\DataSet\Covid19 Positive\0a7faa2a.jpg"
         f.save(file_path)
         outcome=predict_covid(file_path)
-        return str(outcome)
+        return render_template('success.html', prediction=outcome,url=file_path)
     return render_template("index.html")
 
 if __name__=='__main__':
